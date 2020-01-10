1468558e5846adcd2392781fbefbab6bb15d1a4b
sampada wattamwar <sampadawattamwarp@gmail.com>
2020-01-10 18:58:59 +0530

    work on ass project and accessories to emp

--- src/actions/inventoryAdminAction.js
+++ src/actions/inventoryAdminAction.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 import { BASE_URL } from "../components/common/config";
+
 export const GET_ADMINDASHBOARDDETAILS = "GET_ADMINDASHBOARDDETAILS";
-
 export const GETEMPDETAIL = 'GETEMPDETAIL';
-
+export const CREATEEMPLOYEEDETAIL = 'CREATEEMPLOYEEDETAIL';
+export const GETACCESSORIES = 'GETACCESSORIES';
+export const GETPROJECTES = 'GETPROJECTES';
 export function getEmployeeList(data) {
 
   const API_CONFIG = {
@@ -71,3 +73,73 @@ export function getAdminDashboardData(data) {
   };
 }
 
+
+export function createEmployeeDetail(data) {
+
+  const API_CONFIG = {
+    headers: {
+      'Content-Type': 'application/json',
+      // 'Authorization': 'JWT ' + data.token
+    }
+  };
+  let URL = `http://35.154.43.111:9000/company/${data.company_id}/branch/${data.branch_id}/create_user_employee/`;
+  let payload = data.payload
+  return dispatch => {
+    return axios.post(URL, payload, API_CONFIG)
+      .then(function (response) {
+        dispatch({
+          type: CREATEEMPLOYEEDETAIL,
+          payload: response
+        });
+      })
+      .catch(function (error) {
+
+      });
+  }
+}
+
+export function getAccessories(data) {
+
+  let URL = `http://35.154.43.111:9000/company/1/branch/1/accessories`;
+  const API_CONFIG = {
+    headers: {
+      'Content-Type': 'application/json',
+      // 'Authorization': 'JWT ' + branch.token,
+    }
+  };
+  return dispatch => {
+    return axios.get(URL, API_CONFIG)
+      .then(function (response) {
+        dispatch({
+          type: GETACCESSORIES,
+          payload: response,
+        })
+      })
+      .catch(function (error) {
+
+      });
+  }
+}
+
+export function getProjectes(data) {
+
+  let URL = `http://35.154.43.111:9000/company/1/branch/1/get_project_details/`;
+  const API_CONFIG = {
+    headers: {
+      'Content-Type': 'application/json',
+      // 'Authorization': 'JWT ' + branch.token,
+    }
+  };
+  return dispatch => {
+    return axios.get(URL, API_CONFIG)
+      .then(function (response) {
+        dispatch({
+          type: GETPROJECTES,
+          payload: response,
+        })
+      })
+      .catch(function (error) {
+
+      });
+  }
+}
--- src/reducers/inventoryAdminReducer.js
+++ src/reducers/inventoryAdminReducer.js
@@ -1,33 +1,55 @@
 import {
-   GETEMPDETAIL,
-    GET_ADMINDASHBOARDDETAILS
-    
-  } from "../actions/inventoryAdminAction";
-  
-  const initialState = {
-    adminDashboardDetail:null,
-    companyId:1,
-    adminbranchId:1,  
-    employeeDetail:null  
-  };
-  
-  export default function inventoryAdmin(state = initialState, action) {
-    switch (action.type) {
-      
-      case GETEMPDETAIL: {
-        return {
-          ...state,
-          employeeDetail: action.payload
-        };
-      }
-      case GET_ADMINDASHBOARDDETAILS:{
-        return{
-          ...state,
-          adminDashboardDetail: action.payload
-        }
-      }
-      default:
-        return state;
+  GETEMPDETAIL,
+  GET_ADMINDASHBOARDDETAILS,
+  CREATEEMPLOYEEDETAIL,
+  GETACCESSORIES,
+  GETPROJECTES
+} from "../actions/inventoryAdminAction";
+
+const initialState = {
+  adminDashboardDetail: null,
+  employeeDetail: null,
+  createEmp:null,
+  accessories:null,
+  projectes:null
+};
+
+export default function inventoryAdmin(state = initialState, action) {
+  switch (action.type) {
+
+    case GETEMPDETAIL: {
+      return {
+        ...state,
+        employeeDetail: action.payload
+      };
     }
+
+    case GET_ADMINDASHBOARDDETAILS: {
+      return {
+        ...state,
+        adminDashboardDetail: action.payload
+      }
+    }
+
+    case CREATEEMPLOYEEDETAIL: {
+      return {
+        ...state,
+        createEmp: action.payload
+      }
+    }
+    case GETACCESSORIES :{
+      return {
+        ...state,
+        accessories:action.payload
+      }
+    }
+    case GETPROJECTES :{
+      return {
+        ...state,
+        projectes:action.payload
+      }
+    }
+    default:
+      return state;
   }
-  
+}
